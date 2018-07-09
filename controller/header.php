a4211d9a0d022700ac75303c0c9c92abbae5dc88
Benjamin TROTIN <benjamin.trotin@gmail.com>
2018-07-10 00:45:05 +0200

    un oublie

--- controller/header.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once("include.php");
-echo $twig->render('view/index.twig', ['applicationName' => "Biblio Paris"]);
---
+++ controller/home.php
@@ -0,0 +1,3 @@
+<?php
+require_once("include.php");
+echo $twig->render('home.twig', ['appName' => "Inventorix 2000"]);

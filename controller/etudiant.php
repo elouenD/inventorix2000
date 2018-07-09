f4bf9307c44630a04552e714355abf9521e86a27
elouen.dumas.pilhou <elouen.dumas.pilhou@schibsted.com>
2018-07-09 16:21:19 +0200

    feature: add nav etudiant

--- controller/etudiant.php
+++ controller/etudiant.php
@@ -1,5 +1,5 @@
 <?php
-require_once("./_lib/vendor/autoload.php");
-$loader = new \Twig_Loader_Filesystem(__DIR__.'/templates');
+require_once("../_lib/vendor/autoload.php");
+$loader = new \Twig_Loader_Filesystem(__DIR__.'/view');
 $twig = new \Twig_Environment($loader);
-echo $twig->render('index.twig', ['name' => "hello "]);
+echo $twig->render('etudiant.twig', ['name' => "hello etudiant"]);

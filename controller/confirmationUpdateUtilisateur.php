0d8847f5511af748fd747c9103ad4132bc55963c
elouen.dumas.pilhou <elouen.dumas.pilhou@schibsted.com>
2018-07-16 16:49:59 +0200

    fix: update utilisateur

--- controller/confirmationUpdateUtilisateur.php
+++ controller/confirmationUpdateUtilisateur.php
@@ -2,10 +2,9 @@
 require_once("include.php");
 include("fonction.php");
 
-echo ($_POST['login']);
 //insertion en base
 if (isset($_POST["mail"]) && isset($_POST["login"])){
-    updateMateriel($_POST['id'], $_POST["mail"],$_POST["login"]);
+    updateUser($_POST['id'], $_POST["mail"],$_POST["login"]);
 }
 
 echo $twig->render('confirmationUtilisateur.twig', ['post' => $_POST]);

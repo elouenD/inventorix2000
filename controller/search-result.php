ee24f204c5bc861c6f16032883ab3c6cf6c7fe4c
noon28 <fouillard.arnaud@gmail.com>
2018-07-16 10:51:18 +0200

    Recherche avec cliquable vers details

--- controller/search-result.php
+++ controller/search-result.php
@@ -4,9 +4,26 @@ include("fonction.php");
 
 
 //fonction de recherche 
+if(isset ($_POST['code']) and ($_POST['code']!='')){
+    $code = findMaterielbyCB($_POST['code']);
+}else{
+    $code[0] = 'Aucun Resultat';
+}
 
-$code = findMaterielbyCB($_POST['code']);
-$desc = findMaterielbyDescription($_POST['description']);
+
+if(isset ($_POST['description']) and ($_POST['description']!='')){
+    $desc = findMaterielbyDescription($_POST['description']);
+}else{
+    $desc[0] = 'Aucun Resultat';
+}
+
+
+if(isset ($_POST['nom']) and ($_POST['nom']!='')){
+    $nom =  findMaterielbyName($_POST['nom']) ;
+}else{
+    $nom[0] = 'Aucun Resultat';
+}
+
 $nom =  findMaterielbyName($_POST['nom']) ;
 
 echo $twig->render('search-result.twig', ['codeBarre'=>$code,'description'=>$desc, 'nom'=>$nom]);

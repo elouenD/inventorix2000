6b8bed4519b456780afef90a00999d174030535f
unknown <nguyen.namphong08@gmail.com>
2018-07-17 08:31:59 +0200

    dernieres modifs

--- controller/utilisateur-detail.php
+++ controller/utilisateur-detail.php
@@ -2,16 +2,20 @@
 require_once("include.php");
 require("fonction.php");
 
-<<<<<<< HEAD
+
 
 
 if(isset($_SESSION['id'])){
     // Set navigation
-    $nav = (object) ['accueil' => false, 'materiel' => false, 'emprunt' => false, 'utilisateur' => true, 'etudiant' => false];
-    
-    $id = $_GET['id'];
-    $userInfo=userInfospec($id);
-    
+    $nav = (object) ['accueil' => false, 'materiel' => true, 'emprunt' => false, 'utilisateur' => false, 'etudiant' => false];
+    if(isset($_GET['idToDelete'])){
+        $idToDelete = $_GET['idToDelete'];
+        echo("test");
+        deleteUser($idToDelete);
+    } else if(isset($_GET['id'])) {
+        $id = $_GET['id'];
+        $userInfo=userInfospec($id);
+    }
     echo $twig->render('utilisateur-detail.twig', ['id' => $id, 'nav' => $nav, 'userInfo'=> $userInfo[0]]);
 }
 else{
@@ -20,17 +24,3 @@ else{
     $session = "Rien";
 }
 
-
-
-
-=======
-if(isset($_GET['idToDelete'])){
-    $idToDelete = $_GET['idToDelete'];
-    echo("test");
-    deleteUser($idToDelete);
-} else if(isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $userInfo=userInfospec($id);
-}
-echo $twig->render('utilisateur-detail.twig', ['id' => $id, 'nav' => $nav, 'userInfo'=> $userInfo[0]]);
->>>>>>> 7aa114209208ebc18642be35bd5fd0a3cd5af49f
